819000af86557e2d1a3aa3b552c6d9fbf6886cbb
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-12 15:02:23 +0530

    We now limit the tweets we collect using a paramter upon init. Also, we AGAIN filter out tweets after we get them.

--- data/twitter_interface.py
+++ data/twitter_interface.py
@@ -1,14 +1,14 @@
 from twython import Twython, TwythonStreamer
 from settings import APP_KEY, ACCESS_TOKEN
 
-
 class YalgaarTwitterInterface:
     '''gets the data out from Twitter and caches it'''
     
     hashtag = '#SaveTheInternet'
     t = None
+    limit = 100 #The number of tweets we will collect
 
-    def __init__(self, hashtag = None):
+    def __init__(self, hashtag = None, limit = 50):
         '''
         :hashtag: the hashtag for searching
         '''
@@ -16,21 +16,36 @@ class YalgaarTwitterInterface:
         if hashtag:
             self.hashtag = hashtag
         
+        self.limit = limit
+
         self.t = Twython(APP_KEY, access_token=ACCESS_TOKEN)
         
     def get_data(self):
 
-        results = self.t.search_gen(
-                    self.hashtag,
-                    count       = 100, 
-                    result_type =  'mixed'
+        results = self.t.cursor(self.t.search,
+                    q           = self.hashtag,
+                    count       = (self.limit + 100) #this is random
+                    result_type = 'mixed'
                     )
     
         try:
-            for r in results:
-                print r['text']
+            count = 0
+            tweets = {}
 
-            return results
+            for r in results: #results is a GENERATOR!
+                if count >= self.limit:
+                    break;
+
+                #filter out these tweets further
+
+                #we don't want anyone's conversations
+                #we don't want to snoop on someone's conversations
+                if r['in_reply_to_screen_name'] is not None:
+                    continue;
+                else:
+                    tweets[r['text']] = r
+                    count = count + 1
+            return tweets
         except Exception as e:
             raise e
             return False
