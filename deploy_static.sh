fbb6275278797ebcbc1af715a8e7e34de1c3156e
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-16 01:56:24 +0530

    We pull latest changes for gh-pages and also clean up after deploying

--- deploy_static.sh
+++ deploy_static.sh
@@ -9,12 +9,15 @@ source ./yalgaar_env/bin/activate
 python freeze.py
 
 git checkout gh-pages
+git pull origin gh-pages
 
 mv -v ./yalgaar/build/* ./
 
 git add -A --ignore-errors
 git commit -m "Deployment on `date -R`"
 git push origin gh-pages
+
+rm -fr *
 
 git checkout master
 
