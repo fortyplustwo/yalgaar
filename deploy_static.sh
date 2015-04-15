dcffc4d33d03c7492f2753aa9e3680ce002735f3
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-16 01:50:23 +0530

    Deployment on Thu, 16 Apr 2015 01:50:23 +0530

---
+++ deploy_static.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git checkout master
+
+git pull --rebase origin
+
+source ./yalgaar_env/bin/activate
+
+python freeze.py
+
+git checkout gh-pages
+
+mv -v ./yalgaar/build/* ./
+
+git add -A --ignore-errors
+git commit -m "Deployment on `date -R`"
+git push origin gh-pages
+
+git checkout master
+
