e682577f9a7bcc0f0daa72d6f35c4095dda437e4
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-16 00:46:59 +0530

    We now collect fresh tweets everytime before generating a frozen site!

--- freeze.py
+++ freeze.py
@@ -1,7 +1,14 @@
 from flask.ext.frozen import Freezer
-from yalgaar import app
+from yalgaar.yalgaar import app
+from yalgaar.data.collect_tweets import collect_tweets
 
 f = Freezer(app)
+
 if __name__ == '__main__':
+    #first get fresh tweets
+    collect_tweets(tweet_type = 'recent', hashtag = '#SaveTheInternet', limit = 100)
+    collect_tweets(tweet_type = 'popular', hashtag = '#SaveTheInternet', limit = 100)
+
+    #now freeze them!
     f.freeze()
 
