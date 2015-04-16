cc64957815af6aa05111942989deac32bcbd67dc
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2015-04-16 22:20:45 +0530

    We now sort the data using weight and date both when collect from db

--- yalgaar/yalgaar.py
+++ yalgaar/yalgaar.py
@@ -22,7 +22,7 @@ def popular_tweets():
     popular_tweets = s.query('tweet','user','tweet_id','tweeted_on'). \
                           from_statement(text(
                             "select * from tweets where tweet_type = 'popular' \
-                             order by weight desc limit %s" % MAX_POPULAR_TWEETS )).all()
+                             order by weight desc, tweeted_on desc limit %s" % MAX_POPULAR_TWEETS )).all()
 
     return render_template('tweets.html', tweets = popular_tweets, type_of_tweets = 'popular')
 
@@ -40,7 +40,7 @@ def recent_tweets():
                           from_statement(text(
                             "select * from tweets where tweet_type = 'recent' \
                              and tweeted_on >= (now() - interval '12 hours') \
-                             order by weight desc limit %s" % MAX_RECENT_TWEETS)).all()
+                             order by tweeted_on desc, weight desc limit %s" % MAX_RECENT_TWEETS)).all()
 
     return render_template('tweets.html', tweets = recent_tweets, type_of_tweets = 'recent')
 
